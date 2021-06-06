824b87c9882522828de5c639ca4a8abafb683509
Nate <nate@wave.com>
2021-06-06 08:28:59 -0400

    wip tests

---
+++ tests/sendwave/pants_docker/test_package.py
@@ -0,0 +1,16 @@
+
+
+def test_build_tags():
+    assert False
+
+def test_tags_include_repo():
+    assert False
+
+def test_create_dockerfile():
+    assert False
+
+def test_dockerfile_no_workdir():
+    assert False
+
+def test_dockerfile_no_command():
+    assert False
